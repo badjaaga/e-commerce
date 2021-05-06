92f44d937743bb44a82f8ce50c3a7fd45a2e8b06
Nadzeya Molchan <nadzeya_molchan@epam.com>
2021-05-06 14:07:38 +0300

    feat: add editing and deleting products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -16,12 +16,15 @@ router.get('/admin/products', requireAuth, async (req, res) => {
     res.send(productsIndexTemplate({products}));
 });
 
-router.get('/admin/products/new', requireAuth, (req, res) => {
+router.get('/admin/products/new',
+    requireAuth,
+    (req, res) => {
     res.send(productsNewTemplate({}));
 });
 
 router.post('/admin/products/new',
-    requireAuth, upload.single('image'), [requireTitle, requirePrice],
+    requireAuth, upload.single('image'),
+    [requireTitle, requirePrice],
     handleErrors(productsNewTemplate),
     async (req, res) => {
         const image = req.file.buffer.toString('base64');
@@ -30,16 +33,44 @@ router.post('/admin/products/new',
         res.redirect('/admin/products');
     });
 
-router.get('/admin/products/:id/edit', async (req,res) => {
-    const product = await productsRepo.getOneBy(req.params.id);
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+    const product = await productsRepo.getOne(req.params.id);
 
-    if(!product){
-        return res.send('product not found');
+    if (!product) {
+        return res.send('Product not found');
     }
-    res.send(productsEditTemplate({product}));
-} )
 
-router.post('/admin/products/:id/edit', requireAuth, async(req,res) => {
+    res.send(productsEditTemplate({ product }));
+});
 
+router.post(
+    '/admin/products/:id/edit',
+    requireAuth,
+    upload.single('image'),
+    [requireTitle, requirePrice],
+    handleErrors(productsEditTemplate, async (req) => {
+        const product = await productsRepo.getOne(req.params.id);
+        return {product};
+    }),
+    async (req, res) => {
+        const changes = req.body;
+
+        if (req.file) {
+            changes.image = req.file.buffer.toString('base64');
+        }
+
+        try {
+            await productsRepo.update(req.params.id, changes);
+        } catch (err) {
+            return res.send('Could not find item');
+        }
+
+        res.redirect('/admin/products');
+    }
+);
+
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) =>{
+    await productsRepo.delete(req.params.id);
+    res.redirect('/admin/products');
 })
 module.exports = router;
